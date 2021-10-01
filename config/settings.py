0316cfdb204b9420a7047f6ea37d44742362a195
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-10-02 04:40:44 +0800

    Try fixing workflow

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import sys, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,19 +75,27 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        # "ENGINE": "django.db.backends.mysql",
-        # "NAME": "pandemic_project",
-        # "USER": "mariadb",
-        # "PASSWORD": "mariadb",
-        # "DEFAULT_CHARSET": "utf-8",
-        # "HOST": "db",
-        # "PORT": 3306,
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if sys.argv[1] == "test":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            # "ENGINE": "django.db.backends.mysql",
+            # "NAME": "pandemic_project",
+            # "USER": "mariadb",
+            # "PASSWORD": "mariadb",
+            # "DEFAULT_CHARSET": "utf-8",
+            # "HOST": "db",
+            # "PORT": 3306,
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
