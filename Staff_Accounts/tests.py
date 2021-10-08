215205ccf9837c5dfa83b0ba0d9f89927ec22f10
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-10-08 18:48:33 +0800

    Update test cases

--- Staff_Accounts/tests.py
+++ Staff_Accounts/tests.py
@@ -1,51 +1,62 @@
-# from Staff_Accounts.models import CustomUser
+from django.contrib.auth import get_user_model
+from Staff_Accounts.models import Staff
 from django.test import TestCase
 
 # Create your tests here.
-# class UserModels(TestCase):
-#    def test_create_user(self):
-#        User = CustomUser
-#        user = User.objects.create_user(
-#            username="Joshua",
-#            email="loyo0420@gmail.com",
-#            password="xA12$$fdg2356!f5",
-#            roles="contact tracers",
-#            email_validated=True,
-#        )
-#        self.assertEqual(user.email, "loyo0420@gmail.com")
-#        self.assertTrue(user.is_active)
-#        self.assertFalse(user.is_staff)
-#        self.assertFalse(user.is_superuser)
+class CreateUser(TestCase):
+    def test_create_user(self):
+        user = get_user_model().objects.create_user(
+            email="testdata@testing.com",
+            password="xA12$$fdg2356!f5",
+        )
+        Staff.objects.create(
+            user=user,
+            roles="contact tracers",
+            email_validated=True,
+        )
+        self.assertEqual(user.email, "testdata@testing.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
 
 
-# class LogInTest(TestCase):
-#    def setUp(self) -> None:
-#        User = CustomUser
-#        user = User.objects.create_user(
+class LogInTest(TestCase):
+    def setUp(self) -> None:
+        user = get_user_model().objects.create_user(
+            email="testdata@testing.com",
+            password="xA12$$fdg2356!f5",
+        )
+        Staff.objects.create(
+            user=user,
+            roles="contact tracers",
+            email_validated=True,
+        )
 
-#            username="Joshua",
-#            email="loyo0420@gmail.com",
-#            password="xA12$$fdg2356!f5",
-#            roles="contact tracers",
-#            email_validated=True,
-#        )
+    def test_Login_Invalid_Username_Invalid_Password(self):
+        self.credentials = {"email": "testuser@testuser.com", "password": "secret"}
+        # send login data
+        response = self.client.post("", self.credentials, follow=True)
 
-#   def test_Login_Invalid_Username_Invalid_Password(self):
-#       self.credentials = {"username": "testuser", "password": "secret"}
-#       # send login data
-#       response = self.client.post("", self.credentials, follow=True)
+        self.assertRaises(KeyError, response.context.__getitem__, "user")
 
-# print(response.context["user"].password)
-#       self.assertFalse(
-#           response.context["user"].is_authenticated
-#       )  # Assert user cannot log in
+    def test_Login_Valid_Username_Valid_Password(self):
+        self.credentials = {
+            "email": "testdata@testing.com",
+            "password": "xA12$$fdg2356!f5",
+        }
+        # send login data
+        response = self.client.post("", self.credentials, follow=True)
 
-#   def test_Login_Valid_Username_Valid_Password(self):
-#       self.credentials = {"username": "Joshua", "password": "xA12$$fdg2356!f5"}
-# send login data
-#       response = self.client.post("", self.credentials, follow=True)
+        self.assertTrue(
+            response.context["user"].is_authenticated
+        )  # Assert user can log in
 
-# print(response.context["user"].password)
-#       self.assertTrue(
-#           response.context["user"].is_authenticated
-#       )  # Assert user cannot log in
+    def test_Login_Valid_Username_Invalid_Password(self):
+        self.credentials = {
+            "email": "testdata@testing.com",
+            "password": "xA12$$fdg2356!f6",
+        }
+        # send login data
+        response = self.client.post("", self.credentials, follow=True)
+
+        self.assertRaises(KeyError, response.context.__getitem__, "user")
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,6 +80,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "pandemic_project",
         "USER": "mariadb",
+        "TEST": {
+            "MIRROR": "default",
+        },
         "PASSWORD": "mariadb",
         "DEFAULT_CHARSET": "utf-8",
         "HOST": "127.0.0.1",
