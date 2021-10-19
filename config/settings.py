39742a5fbcae5c4c4ef3da2d410ee2499c9cfef5
LyuJiawen <gwendoline0812@gmail.com>
2021-10-19 15:37:48 +0800

    Add knox to app

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "knox",
     "issuer",
     "tracer",
     "official.apps.OfficialConfig",
