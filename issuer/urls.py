8ecbfe3e91bac11343c9225a7c2d95b58b76ae2f
LyuJiawen <gwendoline0812@gmail.com>
2021-10-22 21:51:45 +0800

    Fixed bugs for issuer interface and removed useless comments.

--- issuer/urls.py
+++ issuer/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("issue_token/", views.issue_token, name="issue_token"),
     path("inactivate_token/", views.inactivate_token, name="inactivate_token"),
-    # path("inact_result/", views.inact_result, name="inactivate_result"),
     path("error_message/", views.error_message, name="error_message"),
 ]
