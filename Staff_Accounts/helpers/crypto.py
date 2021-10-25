ab5647f1c886f4b58810b2e13ee7c1690a883ec9
Hemrish Bundhoo <e0407840@u.nus.edu>
2021-10-25 16:02:18 +0800

    Ensure Activation Link is collision resistant

--- Staff_Accounts/helpers/crypto.py
+++ Staff_Accounts/helpers/crypto.py
@@ -1,7 +1,8 @@
 import hashlib, logging
 from django.utils.crypto import get_random_string
+from Staff_Accounts.models import Staff
 
-from config.settings import DB
+from config.settings import DB, SECRET_KEY
 
 db_logger = logging.getLogger(DB)
 
@@ -13,8 +14,17 @@ def pg_records(request, list, num):
 def generate_activation_key(email):
     db_logger.info("generate_activation_ley")
     chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
-    secret_key = get_random_string(20, chars)
-    return hashlib.sha256((secret_key + email).encode("utf-8")).hexdigest()
+    while True:
+        saltPre = get_random_string(20, chars)
+        saltPost = get_random_string(20, chars)
+        activation_key = hashlib.sha256(
+            (saltPre + SECRET_KEY + email + saltPost).encode("utf-8")
+        ).hexdigest()
+        try:
+            new_session_user = Staff.objects.get(activation_key=activation_key)
+        except:
+            break
+    return activation_key
 
 
 def generate_otp():
